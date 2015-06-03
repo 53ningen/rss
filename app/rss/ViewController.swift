9fb507f6f40c21aea69f57cabbfe90230ddaf606
53nigen <korun24276@gmail.com>
2015-06-03 22:50:09 +0900

    send and receive data between different view controllers

--- app/rss/ViewController.swift
+++ app/rss/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var topTextField: UITextField!
 
+    @IBOutlet weak var topLabel: UILabel!
+    
+//    @IBAction func topButton(sender: AnyObject) {
+//        topLabel.text = "hoge"
+//        topLabel.textColor = UIColor.blueColor()
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +27,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let godViewController: GodViewController = segue.destinationViewController as! GodViewController
+        godViewController.godField = self.topTextField.text        
+    }
 
 }
