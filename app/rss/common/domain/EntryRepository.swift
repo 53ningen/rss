2c99c92bfd11675cd20d078ffb4ef9179ac77a9a
53nigen <korun24276@gmail.com>
2015-06-10 15:23:51 +0900

    refactoring

--- app/rss/common/domain/EntryRepository.swift
+++ app/rss/common/domain/EntryRepository.swift
@@ -8,9 +8,19 @@
 import Foundation
 
 public class EntryRepository {
+    
+    static let endpoint: NSURL = NSURL(string: "http://qiita.com/tags/swift/feed.atom")!
+    
+    public func getEntries(callback: ([Entry]?) -> Void) {
+        func go(res: NSURLResponse?, data: NSData?, error: NSError?) {
+            callback(EntryConverter.parseData(data!))
+        }
+        let req = NSURLRequest(URL: EntryRepository.endpoint)
+        self.get(req, handler: go)
+    }
 
-    public func getEntries() -> [Entry] {
-        return []
+    func get(request: NSURLRequest, handler: (res: NSURLResponse!, data: NSData!, error: NSError!) -> Void) {
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: handler)
     }
     
 }
--- app/rss/ui/ViewController.swift
+++ app/rss/ui/ViewController.swift
@@ -9,7 +9,13 @@ import RxSwift
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSXMLParserDelegate {
-    
+
+    var entries: [Entry] = []
+    let entryRepo = EntryRepository()
+    @IBOutlet weak var tableViewCell: UITableViewCell!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    @IBOutlet var progressView: UIProgressView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
@@ -20,73 +26,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBAction func reloadEntries(sender: AnyObject) {
         getEntries()
-        tableView.reloadData()
     }
     
     func getEntries() {
         view.addSubview(progressView)
         self.progressView.setProgress(0.2, animated: true)
-        let url: NSURL = NSURL(string: "http://qiita.com/tags/swift/feed.atom")!
-        let parser: NSXMLParser = NSXMLParser(contentsOfURL: url)!
-        parser.delegate = self
-        parser.parse()
+        entryRepo.getEntries(getEntries)
         self.progressView.setProgress(1.0, animated: true)
         self.progressView.removeFromSuperview()
-        
     }
     
-    var token: String = ""
-    var entries: [Entry] = []
-    var currentElement: String? = nil
-    var entryBuilder: EntryBuilder = EntryBuilder()
-    
-    func parserDidStartDocument(parser: NSXMLParser) {
-    }
-    
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
-        switch elementName {
-        case "entry":
-            currentElement = elementName
-        default:
-            break
+    func getEntries(entries: [Entry]?) -> Void {
+        if let e = entries {
+            self.entries = e
         }
-        token = ""
-    }
-    
-    func parser(parser: NSXMLParser, foundCharacters string: String?) {
-        if let s = string {
-            token = token + s
-        }
-    }
-    
-    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        if let elem = currentElement {
-            switch elementName {
-            case "entry":
-                entries.append(entryBuilder.build())
-                currentElement = nil
-            case "id":
-                entryBuilder.id = token
-            case "name":
-                entryBuilder.author = token
-            case "published":
-                entryBuilder.published = token
-            case "updated":
-                entryBuilder.updated = token
-            case "url":
-                entryBuilder.url = NSURL(string: token)
-            case "title":
-                entryBuilder.title = token
-            case "content":
-                break
-            default:
-                break
-            }
-        }
-        token = ""
-    }
-
-    func parserDidEndDocument(parser: NSXMLParser) {
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -115,11 +69,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var targetUrl: NSURL? = NSURL(string: "http://gochiusa.com")
     
-    @IBOutlet weak var tableViewCell: UITableViewCell!
-
-    @IBOutlet weak var reloadButton: UIBarButtonItem!
-    
-    @IBOutlet var progressView: UIProgressView!
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "cellSegue" {
